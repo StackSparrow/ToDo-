2ca6b1405f9187bd5528277a3a57d5da7a5b20ec
marian.koenig@obcc.de <mariankoenig@mks-imac.local>
2019-01-17 14:07:29 +0100

    Initial Commit

---
+++ ToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Marian König on 17.01.19.
+//  Copyright © 2019 none. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
